ebb2efb49380b1d9bb3e5a5e5b9de27d49469de5
Chris Monogios <chr_mon_@hotmail.com>
2016-01-09 18:28:54 +0100

    init commit for Regex with javascript

---
+++ Regex/Introduction/solutions.js
@@ -0,0 +1,17 @@
+//Challenge: Matching Specific String
+var Regex_Pattern = 'hackerrank';
+
+//Challenge: Matching Anything But a Newline
+var Regex_Pattern = '\.{3}\\.\.{3}\\.\.{3}\\.\.{3}';
+
+//Challenge: Matching Digits & Non-Digit Characters
+var Regex_Pattern = /\d{2}\D\d{2}\D\d{4}/;
+
+//Challenge: Matching Whitespace & Non-Whitespace Character
+var Regex_Pattern = /\S{2}\s\S{2}\s\S{2}/;
+
+//Challenge: Matching Word & Non-Word Character
+var Regex_Pattern = /\w{3}\W\w{10}\W\w{3}/;
+
+//Challenge: Matching Start & End
+var Regex_Pattern = /^\d\w{4}.$/;
