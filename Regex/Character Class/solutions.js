5fbb373f4c365305780c046eb88e9e36e8d63d61
Chris Monogios <chr_mon_@hotmail.com>
2016-01-13 08:24:22 +0100

    new solutions for the Regex Domain

---
+++ Regex/Character Class/solutions.js
@@ -0,0 +1,8 @@
+//Challenge: Matching Specific Characters
+var Regex_Pattern = /^[123][120][xs0][30Aa][xsu][.,]$/;
+
+//Challenge: Excluding Specific Characters
+var Regex_Pattern = /^[^0-9][^aeiou][^bcDF][^\s\r\n\t\f][^AEIOU][^\.,]$/;
+
+//Challenge: Matching Range Of Characters
+var Regex_Pattern = /^[a-z][1-9][^a-z][^A-Z][A-Z]/;
